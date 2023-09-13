718b8a6b112636aa085e3d8000a179ebbcc248a6
ruk91 <sajithkasn@gmail.com>
2023-09-13 21:47:30 +0200

    Add recoil, luxon, date picker and drop down

--- App.tsx
+++ App.tsx
@@ -3,16 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {HomeScreen} from "./screens/HomeScreen";
 import {SettingsScreen} from "./screens/SettingsScreen";
+import {RecoilRoot} from "recoil";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
   );
 }
--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,10 +1,44 @@
 import {Text, View} from "react-native";
 import * as React from "react";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import DropDownPicker from "react-native-dropdown-picker";
+import {useState} from "react";
+import {DateTime} from "luxon";
+import {useRecoilState} from "recoil";
+import {expireDateState} from "../state/expireDateState";
 
 export function SettingsScreen() {
+
+  const [expireDate, setExpireDate] = useRecoilState<DateTime>(expireDateState);
+  const [items, setItems] = useState([
+    { label: "Milk", value: "milk" },
+    { label: "Bread", value: "bread" },
+    { label: "Cheese", value: "cheese" },
+  ]);
+  const [selectedItem, setSelectedItem] = useState<string>(null);
+  const [itemsOpen, setItemsOpen] = useState(false);
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+    <View className="flex-1 justify-center items-center">
+      <View className="flex">
+        <DropDownPicker
+          open={itemsOpen}
+          value={selectedItem}
+          items={items}
+          setOpen={setItemsOpen}
+          setValue={() => {}}
+          placeholder="Select item"
+          onChangeValue={(i) => setSelectedItem(i.value)}
+          zIndex={3000}
+          zIndexInverse={1000}
+        />
+      </View>
+      <Text>Expiration Date</Text>
+      <DateTimePicker
+        className=""
+        accessibilityLabel={"Expiration Date"}
+        value={expireDate?.toJSDate()}
+        onChange={(i) => {setExpireDate(DateTime.fromSeconds(i.nativeEvent.timestamp))}}
+      />
     </View>
   );
 }
